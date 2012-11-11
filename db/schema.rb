4c12b0368283b220392761f2e0139c357c2cfb0e
Jamie Hush <jhush91@gmail.com>
2012-11-11 10:16:16 -0500

    Schema problems

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20121111031605) do
+ActiveRecord::Schema.define(:version => 20121111012151) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -38,12 +39,15 @@ ActiveRecord::Schema.define(:version => 20121111031605) do
     t.string   "email"
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           :default => false
-    t.string   "pettype"
-    t.string   "breed"
-    t.datetime "birthday"
+    t.boolean  "admin",              :default => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
